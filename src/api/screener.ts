8f730f78c39b051235c9fd64a216f4fa14392f35
Xavier Ritch <xavierritch@gmail.com>
2025-05-16 13:03:51 +0100

    fix: update api url

--- src/api/screener.ts
+++ src/api/screener.ts
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import type { UserAnswer } from '../types/screener';
 
-const BASE_URL = 'http://127.0.0.1:8080';
+const API_URL = 'https://blueprint-screener-backend-uk8l.vercel.app/';
 
 export const fetchScreener = async () => {
-  const res = await axios.get(`${BASE_URL}/screener`);
+  const res = await axios.get(`${API_URL}/screener`);
   return res.data;
 };
 
 export const submitAnswers = async (answers: UserAnswer[]) => {
-  const res = await axios.post(`${BASE_URL}/score`, { answers });
+  const res = await axios.post(`${API_URL}/score`, { answers });
   return res.data;
 };
